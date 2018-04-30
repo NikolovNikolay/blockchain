3b408a0c7c0082d30f761f85b238fbc6b853b0e1
Nikolay Nikolov <nikolov89@gmail.com>
2018-05-01 00:46:59 +0300

    Update gitignore and contract deplyment

--- spliiter/migrations/2_deploy_contracts.js
+++ spliiter/migrations/2_deploy_contracts.js
@@ -1,5 +1,3 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Splitter = artifacts.require("./Splitter.sol");
 
 module.exports = function(deployer) {
