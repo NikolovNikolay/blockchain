1d68de0aa0d3fad06f6a22c1c01e36ba4decdfdf
Nikolay Nikolov <nikolov89@gmail.com>
2018-05-01 00:42:31 +0300

    Add 'Splitter' contract functionality

--- spliiter/contracts/Splitter.sol
+++ spliiter/contracts/Splitter.sol
@@ -1,51 +1,170 @@
 pragma solidity ^0.4.18;
 
-/** @title Interruptible interface contract*/
-contract InterruptibleContract {
-    
-    // Indicates whether the contract is available to call
-    bool internal isRunning;
-    
-    /** @dev Will turn the contract in not running state and 
-     *       not able to reflect invocation of functions
-     *  @return currentState if the contract is active
-     **/
-    function interruptContract() internal returns(bool currentState) {
-        isRunning = false;
-        return true;
+/** @title Safe math operations */
+contract SafeMath {
+
+    // constant value for 0
+    uint80 constant NoBalance = uint80(0);
+
+    function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        require(a == 0 || c / a == b);
+        return c;
     }
 
-    /** @dev Will turn the contract in running state and 
-     *       able to reflect invocation of functions
-     *  @return currentState if the contract is active
-     **/
-    function resumeContract() internal returns (bool currentState) {
-        isRunning = true;
-        return currentState;
+    function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        return a - b;
     }
-    
-    /** @dev Will continue execution only if the contract
-     *       is in active state
+
+    function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c>=a && c>=b);
+        return c;
+    }
+
+    function safeHalf(uint256 amount) internal pure returns (uint256 half) {
+
+        require(amount > NoBalance);
+        half = amount / 2;
+        require(amount - half == half);
+        return half;
+    }
+}
+
+
+/** @title Interruptible interface */
+contract Interruptible {
+
+    /** @dev Will turn the contract in not running state and
+     *       not able to reflect invocation of functions
+     *  @return the new state of the contract
      **/
-    modifier ifRunning() {
-        if (!isRunning) {
-            revert();
-        }
+    function interruptContract() public returns(bool) {}
+
+    /** @dev Will turn the contract in running state and
+     *       able to reflect invocation of functions
+     *  @return the new state of the contract
+     **/
+    function resumeContract() public returns (bool) { }
+
+}
+
+/** @title Token wrapper with major components */
+contract Token {
+
+    // Holds the address of the owner
+    address internal owner;
+    address constant NoAddress = address(0);
+
+    struct Contributor {
+        uint256 _balance;
+    }
+
+    // Holds balances for contributors
+    mapping(address => Contributor) contributors;
+
+    /** @dev Will return available contributor's balance
+     *  @return balance balance of contributor
+     **/
+    function balanceOf(address holder) constant public returns(uint256) {
+        return contributors[holder]._balance;
+    }
+
+    /** @dev Will check for valid address
+     *  @param _address address to check
+     **/
+    modifier validAddress(address _address) {
+        require(_address != NoAddress);
+        _;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner);
         _;
     }
 }
 
-contract Splitter is InterruptibleContract {
+/** @title Interruptible token */
+contract InterruptibleToken is Interruptible, Token {
 
-    // Holds the address of the owner
-    address private owner;
+    event LogTokenInterrupted();
+    event LogTokenResumed();
 
-    // Holds balances for contributors
-    mapping(address => uint256) private balances;
+    // Indicates whether the contract is available to call
+    bool internal isRunning;
+
+    function interruptContract()
+    public
+    isOwner
+    returns(bool) {
+        isRunning = false;
+        emit LogTokenInterrupted();
+        return isRunning;
+    }
+
+    function resumeContract()
+    public
+    isOwner
+    returns (bool) {
+        isRunning = true;
+        emit LogTokenResumed();
+        return isRunning;
+    }
+
+    /** @dev Will continue execution only if the contract
+     *       is in active state
+     **/
+    modifier ifRunning() {
+        require(isRunning);
+        _;
+    }
+}
+
+/** @title Amount splitter */
+contract AmmountSplitter is SafeMath, InterruptibleToken {
+
+    event LogSplitAmount(
+        address indexed sender,
+        address indexed firstRecipient,
+        address indexed secondRecipient,
+        uint256 recipientSplitAmount);
+
+    /** @dev Will split the transferred amount between two recipients
+     *  @param firstRecipient first address to add split amount to
+     *  @param secondRecipient seconds address to add split amount to
+     *  @return if success will return true, else will revert
+     */
+    function splitAmount(
+        address firstRecipient,
+        address secondRecipient
+    )
+    public
+    ifRunning
+    payable
+    validAddress(firstRecipient)
+    validAddress(secondRecipient)
+    returns(bool)
+    {
+
+        if (owner == firstRecipient || owner == secondRecipient) {
+            revert();
+        }
+
+        uint256 split = safeHalf(msg.value);
+        contributors[firstRecipient]._balance =
+        safeAdd(contributors[firstRecipient]._balance, split);
+        contributors[secondRecipient]._balance =
+        safeAdd(contributors[secondRecipient]._balance, split);
+        emit LogSplitAmount(msg.sender, firstRecipient, secondRecipient, split);
+        return true;
+    }
+}
+
+contract Splitter is AmmountSplitter {
 
     constructor() public {
         owner = msg.sender;
-
         resumeContract();
     }
 }
